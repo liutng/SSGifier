498f2b88b16f20fcf4916f9d908fa84323791197
Tong Liu <402169950@qq.com>
2022-10-09 22:29:59 -0400

    Removed redundant files and moved source files.
    Added some comments.
    
    Moved source files directly under the root folder.

---
+++ MessagePrinter.h
@@ -0,0 +1,27 @@
+#pragma once
+namespace SDDS
+{
+	static std::string VERSION_NAME = "";
+	static int VERSION_CODE = 1;
+	class MessagePrinter
+	{
+	public:
+		void printHelp();
+		/**
+		 * Print unkown switch error meessage.
+		 */
+		void printUnknownSwitch(std::string switchName);
+		/**
+		 * Print current software's version.
+		 */
+		void printVersion();
+		/**
+		 * Print intput file not found error message.
+		 */
+		void printInputFileNotProvided();
+		/**
+		 * Print empty argument error message.
+		 */
+		void printEmptyArg(std::string switchName);
+	};
+}
